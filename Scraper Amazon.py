ea4b1ab442a3768e54e177a9f7c4083b7f6c6c22
Vincenzo Sammartino <92106079+ilsamaritano@users.noreply.github.com>
2022-05-31 10:40:18 +0200

    Update Scraper Amazon.py
    Aggiornato per lo scaricamento automatico di tutte le recensioni da 1 a 5 stelle.

--- Scraper Amazon.py
+++ Scraper Amazon.py
@@ -8,43 +8,60 @@ import csv
 
 PATH = "C:\Drivers\msedgedriver.exe"
 driver = Edge(PATH)
-
-# Seleziono le recensioni con già selezionate le stelle
-driver.get('https://www.amazon.it/product-reviews/B000J34HN4/ref=acr_dp_hist_1?ie=UTF8&filterByStar=five_star&reviewerType=all_reviews#reviews-filter-bar')
-print("Page title is: %s" % (driver.title))
-# Aspetto il caricamento della pagina, trovo il bottone per accettare la policy dei cookie attraverso l'Xpath e lo clicco
-# time.sleep(1)
-cookiebottone = driver.find_element("xpath", '//*[@id="sp-cc-accept"]')
-cookiebottone.click()
-# Aspetto il nuovo caricamento della pagina ed estraggo i dati che mi interessano
-time.sleep(2)
-
+stelle = ['one', 'two', 'three', 'four', 'five']
 testoelem = []
-for i in range(80, 200):
-    elementi = driver.find_elements(
-        "xpath", '//div[starts-with(@id,"customer_review")]/div[4]')
-    # print(elementi)
-    # Uso il try in modo tale che quando trova i video non blocchi l'estrazione
-    for index, elem in enumerate(elementi):
-        try:
-            elemento = "A" + "5" + \
-                (str(i)+str(index)).zfill(4), "1", "0", elem.__getattribute__("text")
-            testoelem.append(elemento)
-        except:
-            print("Contiene video")
-    # print(testoelem)
-    # Clicca l'elemento per avanzare di pagina dopo aver atteso il caricamento (Se non trova più l'elemento mi fermo, significa che sono finite le pagine)
-    time.sleep(10)
+# Seleziono le recensioni con già selezionate le stelle
+for stella in stelle:
+    driver.get('https://www.amazon.it/product-reviews/8830101680/ref=acr_dp_hist_5?ie=UTF8&filterByStar=' +
+               stella+'_star&reviewerType=all_reviews#reviews-filter-bar')
+    print("Page title is: %s" % (driver.title))
+    # Aspetto il caricamento della pagina, trovo il bottone per accettare la policy dei cookie attraverso l'Xpath e lo clicco
+    # time.sleep(1)
+    if stella == 'one':
+        cookiebottone = driver.find_element("xpath", '//*[@id="sp-cc-accept"]')
+        cookiebottone.click()
+    # Aspetto il nuovo caricamento della pagina ed estraggo i dati che mi interessano
+    time.sleep(2)
 
-    try:
-        driver.find_element(
-            "xpath", "//*[@id='cm_cr-pagination_bar']/ul/li[2]/a").click() #Bottone per avanzamento pagina
-    except:
-        time.sleep(2)
-        print("Esecuzione terminata")
-        break
-    # Esporto i dati sul file CSV con codifica "UTF-8"
-    # print(testoelem)
+    if stella == 'one':
+        codice = range(0, 100)
+        star = 1
+    elif stella == 'two':
+        codice = range(20, 100)
+        star = 2
+    elif stella == 'three':
+        codice = range(40, 100)
+        star = 3
+    elif stella == 'four':
+        codice = range(60, 100)
+        star = 4
+    else:
+        codice = range(80, 100)
+        star = 5
+    for i in codice:
+        elementi = driver.find_elements(
+            "xpath", '//div[starts-with(@id,"customer_review")]/div[4]')
+        # Uso il try in modo tale che quando trova i video non blocchi l'estrazione
+        for index, elem in enumerate(elementi):
+            try:
+                elemento = "A" + str(star) + \
+                    (str(i)+str(index)).zfill(4), elem.__getattribute__("text")
+                testoelem.append(elemento)
+            except:
+                print("Contiene video")
+        # print(testoelem)
+        # Clicca l'elemento per avanzare di pagina dopo aver atteso il caricamento (Se non trova più l'elemento mi fermo, significa che sono finite le pagine)
+        time.sleep(10)
+        try:
+            driver.find_element(
+                "xpath", "//*[@id='cm_cr-pagination_bar']/ul/li[2]/a").click()  # Bottone per andare avanti con le pagine
+        except:
+            time.sleep(2)
+            print("Esecuzione terminata")
+            break
+        # Esporto i dati sul file CSV con codifica "UTF-8"
+        # print(testoelem)
+
 with open("C:\output.csv", 'a', encoding='utf-8') as data_file:
     writer = csv.writer(data_file, delimiter='\t')
     writer.writerow(testoelem)
